1077a495d94cf8ff20537c4ffc7973a8552bcafc
katarighe <m.ighe@hotmail.com>
2023-12-01 16:13:37 +0300

    Create doctors factory

---
+++ spec/factories/doctors.rb
@@ -0,0 +1,10 @@
+FactoryBot.define do
+    factory :doctor do
+      name { 'MyString' }
+      city { 'MyString' }
+      specialization { 'MyString' }
+      cost_per_day { 1 }
+      description { 'MyText' }
+    end
+  end
+  
