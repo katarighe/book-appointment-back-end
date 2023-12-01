53bd718c5b3bfa4870abd62c764ee373a9ea3823
katarighe <m.ighe@hotmail.com>
2023-12-01 16:04:46 +0300

    Create Doctors table

--- db/migrate/20231201122318_create_doctors.rb
+++ db/migrate/20231201122318_create_doctors.rb
@@ -1,6 +1,11 @@
-class CreateDoctors < ActiveRecord::Migration[7.1]
+class CreateDoctors < ActiveRecord::Migration[7.0]
   def change
     create_table :doctors do |t|
+      t.string :name
+      t.string :city
+      t.string :specialization
+      t.integer :cost_per_day
+      t.text :description
 
       t.timestamps
     end
