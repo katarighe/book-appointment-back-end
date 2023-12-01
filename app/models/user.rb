ac34402fc57255ae412c626f22cc5832c2d43ac1
katarighe <m.ighe@hotmail.com>
2023-12-01 15:52:36 +0300

    Create User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
-end
+    # Include default devise modules. Others available are:
+    # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+    devise :database_authenticatable, :registerable,
+           :recoverable, :rememberable, :validatable
+  
+    # relationships
+    has_one_attached :image, dependent: :destroy
+    has_many :appointments, dependent: :destroy
+  end
+  
