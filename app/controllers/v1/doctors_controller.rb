c1900cde6a87b99ff08592c63f44ebad14eeec6e
katarighe <m.ighe@hotmail.com>
2023-12-02 19:13:12 +0300

    Reverse back to the doctors

--- app/controllers/v1/doctors_controller.rb
+++ app/controllers/v1/doctors_controller.rb
@@ -1,10 +1,7 @@
 class V1::DoctorsController < ApplicationController
   def index; end
 
-  def show
-    @doctor = Doctor.find(params[:id])
-    render json: DoctorSerializer.new(@doctor).serializable_hash[:data][:attributes], status: :ok
-  end
+  def show; end
 
   def new; end
 
