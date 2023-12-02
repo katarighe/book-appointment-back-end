bdb159856952f441fe47322c941be81185931835
katarighe <m.ighe@hotmail.com>
2023-12-02 19:30:56 +0300

    Update the doctors controller

--- app/controllers/v1/doctors_controller.rb
+++ app/controllers/v1/doctors_controller.rb
@@ -1,7 +1,10 @@
 class V1::DoctorsController < ApplicationController
   def index; end
 
-  def show; end
+  def show
+    @doctor = Doctor.find(params[:id])
+    render json: DoctorSerializer.new(@doctor).serializable_hash[:data][:attributes], status: :ok
+  end
 
   def new; end
 
