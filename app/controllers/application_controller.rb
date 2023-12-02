0b62e76963d7c091cba9c80d8a7b3904927f3e1f
katarighe <m.ighe@hotmail.com>
2023-12-02 19:31:42 +0300

    Update the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,12 @@
 class ApplicationController < ActionController::API
-  ActiveRecord::RecordNotFound
+  #   protect_from_forgery with: :exception
+  include RecordNotFound
 
   before_action :update_allowed_parameters, if: :devise_controller?
+
+  def not_found
+    render json: { error: 'not_found' }
+  end
 
   def authorize_request
     header = request.headers['Authorization']
@@ -9,8 +14,21 @@ class ApplicationController < ActionController::API
     begin
       @decoded = JsonWebToken.decode(header)
       @current_user = User.find(@decoded[:user_id])
-    rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
+    rescue ActiveRecord::RecordNotFound => e
+      render json: { errors: e.message }, status: :unauthorized
+    rescue JWT::DecodeError => e
       render json: { errors: e.message }, status: :unauthorized
     end
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :password, :password_confirmation, :email)
+    end
+    # devise_parameter_sanitizer.permit(:account_update) do |u|
+    #   u.permit(:Name, :Bio, :Photo, :password, :password_confirmation, :email, :current_password)
+    # end
   end
 end
