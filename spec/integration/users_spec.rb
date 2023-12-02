7a9d50b1e69dff81100e7489183afb6f1bc28165
katarighe <m.ighe@hotmail.com>
2023-12-02 19:20:10 +0300

    Update the User spec integration test file

--- spec/integration/users_spec.rb
+++ spec/integration/users_spec.rb
@@ -1,102 +1,102 @@
-require 'swagger_helper'
+require 'rails_helper'
 
-RSpec.describe 'v1/users', type: :request do
-  describe 'Users Login API' do
-    path '/v1/users/login' do
-      post 'Login to get user details and jwt token' do
-        tags 'Users'
-        consumes 'application/json'
-        parameter name: :params, in: :body, schema: {
-          type: :object,
-          properties: {
-            email: { type: :string },
-            password: { type: :string }
-          },
-          require: %w[email password]
-        }
-        response '200', 'user logged in' do
-          @user = FactoryBot.create(:user)
-          email = @user.email
-          let(:params) { { email: email, password: 'password' } }
-          run_test! do |response|
-            data = JSON.parse(response.body)
-            expect(data['token']).not_to be_empty
-            expect(data['exp']).not_to be_empty
-            expect(data['user_details']).not_to be_empty
-          end
-        end
+RSpec.describe 'v1/users/', type: :request do
+  describe 'POST /create' do
+    context 'with valid parameters' do
+      it 'return the token and user info' do
+        post v1_users_signup_path, params: { name: 'test', email: 'test@test.com', password: 'helloWORLD' }
+        body = response.parsed_body
+        expect(body['token']).to_not be nil
+        expect(body['exp']).to_not be nil
+        expect(body['user_details']).to_not be nil
+        expect(body['user_details']['email']).to eq('test@test.com')
+        expect(body['user_details']['name']).to eq('test')
+      end
+    end
 
-        response '401', 'User does not exist' do
-          let(:params) { { email: 'johndoe@gmail.com', password: 'password' } }
-          run_test! do |response|
-            data = JSON.parse(response.body)
-            expect(data['error_message'][0]).to eq('User does not exist')
-          end
-        end
+    context 'with invalid parameters : User with the same email already created' do
+      it 'return message error' do
+        user = User.create(name: 'test', email: 'test@test.com', password: 'helloWORLD')
+        post v1_users_signup_path, params: { name: 'test', email: 'test@test.com', password: 'helloWORLD' }
+        body = response.parsed_body
+        expect(body['error']).to eq('unauthorized')
+        expect(body['error_message']['email']).to eq(['has already been taken'])
+        user.destroy
+      end
+    end
 
-        response '401', 'Invalid password' do
-          @user = FactoryBot.create(:user)
-          email = @user.email
-          let(:params) { { email: email, password: 'invalidpassword' } }
-          run_test! do |response|
-            data = JSON.parse(response.body)
-            expect(data['error_message'][0]).to eq('invalid password')
-          end
-        end
+    context 'with invalid parameters : Name is nil' do
+      it 'return message error' do
+        post v1_users_signup_path, params: { name: nil, email: 'test@test.com', password: 'helloWORLD' }
+        body = response.parsed_body
+        expect(body['error']).to eq('unauthorized')
+        expect(body['error_message']['name']).to eq(['can\'t be blank', 'is too short (minimum is 3 characters)'])
+      end
+    end
+
+    context 'with invalid parameters : Email is nil' do
+      it 'return message error' do
+        post v1_users_signup_path, params: { name: 'test', email: nil, password: 'helloWORLD' }
+        body = response.parsed_body
+        expect(body['error']).to eq('unauthorized')
+        expect(body['error_message']['email']).to eq(['can\'t be blank'])
+      end
+    end
+
+    context 'with invalid parameters : Password is nil' do
+      it 'return message error' do
+        post v1_users_signup_path, params: { name: 'test', email: 'test@test.com', password: '' }
+        body = response.parsed_body
+        expect(body['error']).to eq('unauthorized')
+        expect(body['error_message']['password']).to eq(['can\'t be blank', 'is too short (minimum is 6 characters)'])
+      end
+    end
+
+    context 'with more parameters :' do
+      it 'return the token and user info, others parameters are ignored' do
+        post v1_users_signup_path,
+             params: { name: 'test', email: 'test@test.com', password: 'helloWORLD', confirm_password: 'hello' }
+        body = response.parsed_body
+        expect(body['token']).to_not be nil
+        expect(body['exp']).to_not be nil
+        expect(body['user_details']).to_not be nil
+        expect(body['user_details']['email']).to eq('test@test.com')
+        expect(body['user_details']['name']).to eq('test')
+        expect(body['user_details']['confirm_password']).to be nil
       end
     end
   end
 
-  describe 'Users Signup API' do
-    path '/v1/users/signup' do
-      post 'Creates a user' do
-        tags 'Users'
-        consumes 'application/json', 'application/xml'
-        parameter name: :user, in: :body, schema: {
-          type: :object,
-          properties: {
-            name: { type: :string },
-            email: { type: :string },
-            password: { type: :string }
-          },
-          required: %w[name email password]
-        }
+  describe 'POST /login' do
+    before(:all) do
+      @user = FactoryBot.create(:user)
+    end
+    context 'with valid parameters' do
+      it 'return the token and user info' do
+        post '/v1/users/login', params: { email: @user.email, password: 'password' }
+        body = response.parsed_body
+        expect(body['token']).to_not be nil
+        expect(body['exp']).to_not be nil
+        expect(body['user_details']).to_not be nil
+        expect(body['user_details']['email']).to eq(@user.email)
+      end
+    end
 
-        response '200', 'user created' do
-          let(:user) { { name: 'John Doe', email: 'test@gmail.com', password: '123456' } }
-          schema type: :object,
-                 properties: {
-                   token: { type: :string },
-                   exp: { type: :string },
-                   user_details: { type: :object,
-                                   properties: {
-                                     id: { type: :integer },
-                                     email: { type: :string },
-                                     created_at: { type: :string },
-                                     updated_at: { type: :string },
-                                     name: { type: :string },
-                                     role: { type: :string }
-                                   } }
+    context 'with invalid email : User tried to login with email that doesnt exist' do
+      it 'return user does not exist' do
+        post '/v1/users/login', params: { email: 'johndoe@test.com', password: 'helloWORLD' }
+        body = response.parsed_body
+        expect(body['error']).to eq('unauthorized')
+        expect(body['error_message'][0]).to eq('User does not exist')
+      end
+    end
 
-                 }
-          run_test!
-        end
-
-        response '401', 'invalid request' do
-          let(:user) { { name: 'foo' } }
-          schema type: :object,
-                 properties: {
-                   error: { type: :string },
-                   error_message: { type: :object,
-                                    properties: {
-                                      email: { type: :array },
-                                      password: { type: :array },
-                                      name: { type: :array }
-                                    } }
-
-                 }
-          run_test!
-        end
+    context 'with invalid password : User exists but password is invalid' do
+      it 'return invalid password' do
+        post '/v1/users/login', params: { email: @user.email, password: 'wrongpassword' }
+        body = response.parsed_body
+        expect(body['error']).to eq('unauthorized')
+        expect(body['error_message'][0]).to eq('invalid password')
       end
     end
   end
