9511f788de1524493d427f2f6e4c5d70b13510bf
katarighe <m.ighe@hotmail.com>
2023-12-02 19:16:25 +0300

    Update the DB seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,3 +10,19 @@
 admin_user = User.create(name: "Admin User", email: "admin@microverse.com", password: "password", role: "admin")
 view_user1 = User.create!(name: "Antoine", email: "antoine@microverse.com", password: "password")
 view_user2 = User.create!(name: "Mohamed", email: "mighe@microverse.com", password: "password")
+
+admin_user.image.attach(io: File.open("#{Rails.root}/public/users/194934.png"), filename: '194934.png', content_type: 'image/png')
+view_user1.image.attach(io: File.open("#{Rails.root}/public/users/194935.png"), filename: '194935.png', content_type: 'image/png')
+view_user2.image.attach(io: File.open("#{Rails.root}/public/users/194938.png"), filename: '194938.png', content_type: 'image/png')
+
+doctor1 = Doctor.create!(name: "Dr. John Adams", city: 'New York', specialization: 'Cardiology', cost_per_day: 100, description: 'Dr. John Adams is a cardiologist who specializes in heart disease. He is a certified heart surgeon and a member of the American Heart Association.')
+doctor2 = Doctor.create!(name: "Dr. Robert Spear", city: 'New Jearsy', specialization: 'Nevrology', cost_per_day: 70, description: 'Dr. Dr. Robert Spear is a nevrologiest who specializes in heart disease. He is a certified heart surgeon and a member of the American Heart Association.')
+doctor3 = Doctor.create!(name: "Dr. Bob Jones", city: 'Struga', specialization: 'Cardiology', cost_per_day: 150, description: 'Dr. Bob Jones is a cardiologist who specializes in heart disease. He is a certified heart surgeon and a member of the American Heart Association.')
+
+doctor1.image.attach(io: File.open("#{Rails.root}/public/doctors/doctor1.jpg"), filename: 'doctor1.jpg', content_type: 'image/jpg')
+doctor2.image.attach(io: File.open("#{Rails.root}/public/doctors/doctor2.jpg"), filename: 'doctor2.jpg', content_type: 'image/jpg')
+doctor3.image.attach(io: File.open("#{Rails.root}/public/doctors/doctor3.jpg"), filename: 'doctor3.jpg', content_type: 'image/jpg')
+
+apointment1 = Appointment.create!(doctor_id: doctor1.id, user_id: view_user1.id, date_of_appointment: Time.now)
+apointment2 = Appointment.create!(doctor_id: doctor2.id, user_id: view_user1.id, date_of_appointment: Time.now)
+apointment3 = Appointment.create!(doctor_id: doctor3.id, user_id: view_user1.id, date_of_appointment: Time.now)
