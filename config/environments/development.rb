b284687547126389f1cca89f72d3e7209fdd162d
katarighe <m.ighe@hotmail.com>
2023-12-03 09:26:02 +0300

    Update the localhost

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,8 @@
-require 'active_support/core_ext/integer/time'
+require "active_support/core_ext/integer/time"
+
+Rails.application.routes.default_url_options = {
+  host: 'http://localhost:3001'
+}
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
@@ -19,10 +23,10 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
-  if Rails.root.join('tmp/caching-dev.txt').exist?
+  if Rails.root.join("tmp/caching-dev.txt").exist?
     config.cache_store = :memory_store
     config.public_file_server.headers = {
-      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+      "Cache-Control" => "public, max-age=#{2.days.to_i}"
     }
   else
     config.action_controller.perform_caching = false
