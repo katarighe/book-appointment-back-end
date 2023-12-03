21d4f85b1646a7964e79db52795e8530884e7c9b
katarighe <m.ighe@hotmail.com>
2023-12-03 11:02:41 +0300

    Rewrite the application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,41 +1,36 @@
 class ApplicationController < ActionController::API
-  #   protect_from_forgery with: :exception
-  include RecordNotFound
+  include ActionController::MimeResponds
+  include ActionController::ImplicitRender
+  include ActionController::RequestForgeryProtection
+  protect_from_forgery with: :exception
+
+  rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
   def not_found
-    render json: { error: 'not_found' }
+    render json: { error: 'not_found' }, status: :not_found
   end
 
   def authorize_request
     header = request.headers['Authorization']
     header = header.split.last if header
-
-    @decoded = decode_header(header)
-    @current_user = find_user_by_decoded_id(@decoded[:user_id])
-  rescue ActiveRecord::RecordNotFound, JWT::DecodeError => e
-    render json: { errors: e.message }, status: :unauthorized
-  end
-
-  private
-
-  def decode_header(header)
-    JsonWebToken.decode(header)
-  end
-
-  def find_user_by_decoded_id(user_id)
-    User.find(user_id)
+    @decoded = JsonWebToken.decode(header)
+    @current_user = User.find(@decoded[:user_id])
+  rescue ActiveRecord::RecordNotFound => e
+    render_unauthorized(e.message)
+  rescue JWT::DecodeError => e
+    render_unauthorized(e.message)
   end
 
   protected
 
   def update_allowed_parameters
-    devise_parameter_sanitizer.permit(:sign_up) do |u|
-      u.permit(:name, :photo, :password, :password_confirmation, :email)
-    end
-    # devise_parameter_sanitizer.permit(:account_update) do |u|
-    #   u.permit(:Name, :Bio, :Photo, :password, :password_confirmation, :email, :current_password)
-    # end
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :photo, :password, :password_confirmation, :email])
+    # devise_parameter_sanitizer.permit(:account_update, keys: [:Name, :Bio, :Photo, :password, :password_confirmation, :email, :current_password])
+  end
+
+  def render_unauthorized(message)
+    render json: { errors: message }, status: :unauthorized
   end
 end
