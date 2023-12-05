4a824d0963c147d583454e43e548a05689ba2f6d
katarighe <m.ighe@hotmail.com>
2023-12-05 11:07:28 +0300

    Update the app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,10 @@ class ApplicationController < ActionController::API
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
+  def not_found
+    render json: { error: 'not_found' }
+  end
+
   def authorize_request
     header = request.headers['Authorization']
     header = header.split.last if header
@@ -14,6 +18,14 @@ class ApplicationController < ActionController::API
       render json: { errors: e.message }, status: :unauthorized
     rescue JWT::DecodeError => e
       render json: { errors: e.message }, status: :unauthorized
+    end
+  end
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) do |u|
+      u.permit(:name, :photo, :password, :password_confirmation, :email)
     end
   end
 end
