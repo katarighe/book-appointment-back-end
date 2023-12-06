32a78ae4a23caf120db928be7c7062df08e00ce4
katarighe <m.ighe@hotmail.com>
2023-12-06 15:50:59 +0300

    Rewrite the Users contorller and replace new ones

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -10,15 +10,15 @@ class V1::UsersController < ApplicationController
     if @user
       if Password.new(@user.encrypted_password) == params[:password]
         token = JsonWebToken.encode(user_id: @user.id)
-        time = Time.now + 24.hours.to_i
-        render json: { token:,
-                       exp: time.strftime('%m-%d-%Y %H:%M'),
+        time = Time.now + 24.hours.to_f
+        render json: { token: token,
+                       exp: time,
                        user_details: UserSerializer.new(@user).serializable_hash[:data][:attributes] }, status: :ok
       else
         render json: { error: 'unauthorized', error_message: ['invalid password'] }, status: :unauthorized
       end
     else
-      render json: { error: 'unauthorized', error_message: @user.errors }, status: :unauthorized
+      render json: { error: 'unauthorized', error_message: ['User does not exist'] }, status: :unauthorized
     end
   end
 
@@ -26,20 +26,21 @@ class V1::UsersController < ApplicationController
     @user = User.new(signup_params)
     if @user.save
       token = JsonWebToken.encode(user_id: @user.id)
-      time = Time.now + 24.hours.to_i
-      render json: { token:,
-                     exp: time.strftime('%m-%d-%Y %H:%M'),
+      time = Time.now + 24.hours.to_f
+      render json: { token: token,
+                     exp: time,
                      user_details: UserSerializer.new(@user).serializable_hash[:data][:attributes] }, status: :ok
     else
-      render json: { error: 'unauthorized', error_message: @user.errors }, status: :unauthorized
+      render json: { error: 'forbidden', error_message: @user.errors }, status: :forbidden
     end
   end
 
-  private
-
-  def login_params
-    params.permit(:email, :password)
+  def fetch_current_user
+    @user = User.find(@current_user.id)
+    render json: { data: UserSerializer.new(@user).serializable_hash[:data][:attributes] }, status: :ok
   end
+
+  private
 
   def signup_params
     params.permit(:name, :email, :password, :password_confirmation, :image, :image_url)
