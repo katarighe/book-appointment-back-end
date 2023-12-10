f724c019a141cdae258d9ebb3e6f93cffff7fe69
ANTOINE1128 <makdessyantoine@gmail.com>
2023-12-10 21:52:56 +0200

    add a username and doctorname for appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -4,4 +4,7 @@ class Appointment < ApplicationRecord
 
   # validations
   validates :date_of_appointment, presence: true
+  
+  delegate :name, to: :user, prefix: true, allow_nil: true
+  delegate :name, to: :doctor, prefix: true, allow_nil: true
 end
