051b032f6141e2576b0bc93168aea7997fd69b89
ANTOINE1128 <makdessyantoine@gmail.com>
2023-12-11 14:36:58 +0200

    fix default img

--- db/migrate/20231209101746_add_image_url_to_users.rb
+++ db/migrate/20231209101746_add_image_url_to_users.rb
@@ -1,5 +1,5 @@
 class AddImageUrlToUsers < ActiveRecord::Migration[7.1]
   def change
-    add_column :users, :image_url, :string, default: 'https://thumbs.dreamstime.com/z/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg'
+    add_column :users, :image_url, :string, default: 'https://images.unsplash.com/photo-1484069560501-87d72b0c3669?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTM4fHx1c2VyfGVufDB8fDB8fHww'
   end
 end
