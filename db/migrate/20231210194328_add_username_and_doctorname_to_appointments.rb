43b8518c32f0eb8b1f568db7249bd71f60f57773
ANTOINE1128 <makdessyantoine@gmail.com>
2023-12-11 14:00:35 +0200

    Revert 'add a username and doctorname for appointment'

--- db/migrate/20231210194328_add_username_and_doctorname_to_appointments.rb
+++
@@ -1,6 +0,0 @@
-class AddUsernameAndDoctornameToAppointments < ActiveRecord::Migration[7.1]
-  def change
-    add_column :appointments, :username, :string
-    add_column :appointments, :doctorname, :string
-  end
-end
