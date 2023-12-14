5987c6dd46c469270a80c3d6f45ab2b12c88fb0b
ANTOINE1128 <makdessyantoine@gmail.com>
2023-12-14 17:49:02 +0200

    Modify cors.rb to allow all origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins 'http://localhost:5173' # Add the URL of your React frontend
+      origins '*' # Add the URL of your React frontend
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
   end
