27344448b1c02104bb993d130c37c89768e1221e
ANTOINE1128 <makdessyantoine@gmail.com>
2023-12-14 18:17:21 +0200

    config cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*' # Add the URL of your React frontend
+      origins 'http://localhost:5173' ,'http://localhost:3000' # Add the URL of your React frontend
       resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
     end
   end
